e4d7fecdfc11cb3f8dc20a928098b32fe52f87ea
al <eladco@gmail.com>
2014-04-20 20:05:48 +0300

    replaced urllib2 with requests

--- fileDl.py
+++ fileDl.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # 
 #
-#    Copyright 2013 Elad Cohen <eladco@gmail.com>
+#    Elad Cohen <eladco@gmail.com> 
 ##########################################
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ##########################################
 
-import urllib2
+import requests
 import os
 
 ## Download latest results.
