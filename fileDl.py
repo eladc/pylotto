2eda15b531abc64cd295d0aeda1b433f64db2ac8
al <eladco@gmail.com>
2014-04-20 20:02:13 +0300

    replaced urllib2 with requests

--- fileDl.py
+++ fileDl.py
@@ -22,43 +22,31 @@ import os
 
 ## Download latest results.
 
-def csvDl(file_name, uRL):
+def csvDl(u, file_size, file_name):
 
-  u = urllib2.urlopen(uRL)
-  f = open(file_name, 'wb')
-  meta = u.info()
-  file_size = int(meta.getheaders('Content-Length')[0])
-  print 'Downloading: %s Bytes: %s' % (file_name, file_size)
+    print "Downloading: %s - %s" % (file_name, file_size)
 
-  file_size_dl = 0
-  block_sz = 8192
-  while True:
-      buffer = u.read(block_sz)
-      if not buffer:
-        break
+    with open(file_name, 'wb') as f:
+        for chunk in u.iter_content(chunk_size=1024): 
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+                f.flush()
+    f.close)
 
-      file_size_dl += len(buffer)
-      f.write(buffer)
-      status = r'%10d  [%3.2f%%]' % (file_size_dl, file_size_dl * 100. / file_size)
-      status = status + chr(8)*(len(status)+1)
-      print status,
+def fChk(draw): # check for an updated csv
+    print 'Checking for updated csv file...'
+    os.chdir(draw)
+    fname = '%s.csv' % draw
 
-  f.close()
+    old_f_size = os.path.getsize(fname)  # get the file size of an existing csv 
 
-def fChk(draw): #check for an updated csv
-  print 'Checking for updated csv file...'
-  os.chdir(draw)
-  fname = '%s.csv' % draw
-  old_f_size = os.path.getsize(fname)  #get the file size of an existing csv 
+    url = 'http://www.pais.co.il/%s/Pages/last_Results.aspx?download=1' % draw
 
-  url = 'http://www.pais.co.il/%s/Pages/last_Results.aspx?download=1' % draw
+    u = requests.get(url)
+    file_size = int(u.headers['content-length'])
 
-  u = urllib2.urlopen(url)
-  meta = u.info()
-  file_size = int(meta.getheaders('Content-Length')[0])
-
-  if old_f_size == file_size: # compare size of existing file with downloadable file 
-    print 'You have the latest version, no update needed.'
-  else:
-    csvDl(fname, url)
+    if old_f_size == file_size: # compare size of existing file with downloadable file
+        print 'You have the latest version, no update needed.'
+    else:
+        csvDl(u, file_size, fname)
 
